8998740964a635410dcf282acbf6e3f2f90430f3
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-08 10:59:51 +0200

    My commit message

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,156 @@
-let apiProductApi = new TempApi.ProductApi();import TempApi from '../src/index';window.onload = () => {apiProductApi.getAllproduct((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("ijak3").querySelectorAll( "[dataitem='true']" )].filter(
+let apiProductApi = new TempApi.ProductApi();import TempApi from '../src/index';document.getElementById('iwbmpo').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("iwbmpo")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('if7b79').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("if7b79")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('iedd4a').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("iedd4a")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('iz1sei').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("iz1sei")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('ifdh6').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ifdh6")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('ijctv').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ijctv")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('ilegd').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ilegd")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};document.getElementById('i9oxx').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/home/','');
+      if(productId === '/home' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i9oxx")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/home' ;}}});};window.onload = () => {apiProductApi.getAllproduct((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("ijak3").querySelectorAll( "[dataitem='true']" )].filter(
     (element, index, array) =>
     !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
   );const map = new Map();  data.forEach((item,i) => {
